50e338f227b8d9baf0ff8d515795b77fbb94988c
jstrickler <jstrickler@gmail.com>
2020-02-07 15:28:24 -0500

    end of class

--- EXAMPLES/np_read_csv.py
+++ EXAMPLES/np_read_csv.py
@@ -2,9 +2,10 @@
 import numpy as np
 
 a = np.loadtxt(
-    '../DATA/columns_of_numbers.text',
-    usecols=[2, 3],
+    '../DATA/columns_of_numbers.txt',
+    # usecols=[2, 3],
     skiprows=1,
+    dtype=np.int16
 )
 
 print(a)
